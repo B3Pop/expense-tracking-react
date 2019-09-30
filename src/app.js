a4b1947d38008780c501816c09529c0c6fb634de
Brian Gass <briangass3@gmail.com>
2019-09-30 08:48:48 -0400

    Setup react-router

--- src/app.js
+++ src/app.js
@@ -4,10 +4,39 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const Dashboard = () => (
+    <div>
+        Dashboard
+    </div>
+);
+
+const AddNewExpense = () => (
+    <div>
+        Add New Expense
+    </div>
+);
+
+const EditExpense = () => (
+    <div>
+        Edit Expense
+    </div>
+);
+
+const Help = () => (
+    <div>
+        Help
+    </div>
+);
+
 const routes = (
     <BrowserRouter>
-
+        <div>
+            <Route path = "/" component = { Dashboard } exact = { true } />
+            <Route path = "/add" component = { AddNewExpense } />
+            <Route path = "/edit" component = { EditExpense } />
+            <Route path = "/help" component = { Help } />
+        </div>
     </BrowserRouter>
 );
 
-ReactDOM.render(<BrowserRouter />, document.getElementById('app'));
+ReactDOM.render(routes , document.getElementById('app'));
