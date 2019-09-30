21cc15ca1e8765635c5032e2194cea7e1e0485bd
Brian Gass <briangass3@gmail.com>
2019-09-30 08:14:25 -0400

    Initial commit using boilerplate template

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+const routes = (
+    <BrowserRouter>
+
+    </BrowserRouter>
+);
+
+ReactDOM.render(<BrowserRouter />, document.getElementById('app'));
