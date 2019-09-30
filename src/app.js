13635ee589b10d8c0b06e934a1acf54fdaac77a4
Brian Gass <briangass3@gmail.com>
2019-09-30 11:04:52 -0400

    Add linking between routes using Link and NavLink

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -30,20 +30,33 @@ const Help = () => (
 
 const NotFound = () => (
     <div>
-        404 - Page Not Found
+        404 - Page Not Found! - <Link to = "/">Go home</Link>
     </div>
+);
+
+const Header = () => (
+    <header>
+        <h1>Expense Tracking</h1>
+        <NavLink to = "/" activeClassName = "is-active" exact = {true}>Dashboard</NavLink>
+        <NavLink to = "/add" activeClassName = "is-active">Add Expense</NavLink>
+        <NavLink to = "/edit" activeClassName = "is-active">Edit Expense</NavLink>
+        <NavLink to = "/help" activeClassName = "is-active">Help</NavLink>
+    </header>
 );
 
 const routes = (
     <BrowserRouter>
-        <Switch>
-            <Route path = "/" component = { Dashboard } exact = { true } />
-            <Route path = "/add" component = { AddNewExpense } />
-            <Route path = "/edit" component = { EditExpense } />
-            <Route path = "/help" component = { Help } />
-            <Route component = { NotFound }/>
-        </Switch>
+        <div>
+            <Header />
+            <Switch>
+                <Route path="/" component={Dashboard} exact={true} />
+                <Route path="/add" component={AddNewExpense} />
+                <Route path="/edit" component={EditExpense} />
+                <Route path="/help" component={Help} />
+                <Route component={NotFound} />
+            </Switch>
+        </div>
     </BrowserRouter>
 );
 
-ReactDOM.render(routes , document.getElementById('app'));
+ReactDOM.render(routes, document.getElementById('app'));
